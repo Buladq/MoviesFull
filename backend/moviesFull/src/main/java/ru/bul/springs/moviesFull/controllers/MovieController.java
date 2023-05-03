8f704421c8516ec80afecea578037b8edfa1f50e
Buladq <chumakov.bulat@yandex.ru>
2023-05-03 23:23:15 +0300

    add getByid

--- backend/moviesFull/src/main/java/ru/bul/springs/moviesFull/controllers/MovieController.java
+++ backend/moviesFull/src/main/java/ru/bul/springs/moviesFull/controllers/MovieController.java
@@ -5,6 +5,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.bul.springs.moviesFull.DTO.MovieDTO;
@@ -32,6 +33,11 @@ public class MovieController {
     public ResponseEntity<List<MovieDTO>> getAllMovies(){
         return new ResponseEntity<List<MovieDTO>>(movieService.getAll().stream().map(this::convertToMovieDTO).collect(Collectors.toList()),
         HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieDTO> getOneMovie(@PathVariable("id") int id){
+        return new ResponseEntity<MovieDTO>(convertToMovieDTO(movieService.getMovieById(id)),HttpStatus.OK);
     }
 
 
