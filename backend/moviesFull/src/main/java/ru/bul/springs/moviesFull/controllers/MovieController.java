f1531ccfcde3acf0db67b3404ae0de00e48dd386
Buladq <chumakov.bulat@yandex.ru>
2023-05-03 23:15:48 +0300

    add getAllMovies in Movie COntroller

--- backend/moviesFull/src/main/java/ru/bul/springs/moviesFull/controllers/MovieController.java
+++ backend/moviesFull/src/main/java/ru/bul/springs/moviesFull/controllers/MovieController.java
@@ -1,10 +1,45 @@
 package ru.bul.springs.moviesFull.controllers;
 
 
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.bul.springs.moviesFull.DTO.MovieDTO;
+import ru.bul.springs.moviesFull.models.Movie;
+import ru.bul.springs.moviesFull.service.MovieService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/movies")
 public class MovieController {
+
+    private final MovieService movieService;
+
+    private final ModelMapper modelMapper;
+
+    public MovieController(MovieService movieService, ModelMapper modelMapper) {
+        this.movieService = movieService;
+        this.modelMapper = modelMapper;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<MovieDTO>> getAllMovies(){
+        return new ResponseEntity<List<MovieDTO>>(movieService.getAll().stream().map(this::convertToMovieDTO).collect(Collectors.toList()),
+        HttpStatus.OK);
+    }
+
+
+    public MovieDTO convertToMovieDTO(Movie movie){
+        return modelMapper.map(movie,MovieDTO.class);
+    }
+
+    public Movie convertToMovie(MovieDTO movieDTO){
+        return modelMapper.map(movieDTO,Movie.class);
+    }
 }
--- backend/moviesFull/src/main/java/ru/bul/springs/moviesFull/controllers/TestCont.java
+++ backend/moviesFull/src/main/java/ru/bul/springs/moviesFull/controllers/TestCont.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/apik")
 public class TestCont {
 
     private final MovieService movieService;
